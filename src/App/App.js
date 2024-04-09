7659082a8945190aee084e1626f1b5fbe189f973
Erin Kelley <erin.kelley01@gmail.com>
2024-04-09 09:23:31 -0600

    Set state for reservations

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import React from 'react';
 import Reservations from './Reservations';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [allReservations, setAllReservations] = useState([])
 
   useEffect(() => {
     getReservations()
@@ -12,7 +13,7 @@ function App() {
   function getReservations(){
     fetch('http://localhost:3001/api/v1/reservations')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => setAllReservations(data))
   }
 
   return (
