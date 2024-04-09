1b97063fa536f1e45ee8d4328d0c652629f8ad8f
Erin Kelley <erin.kelley01@gmail.com>
2024-04-09 11:04:17 -0600

    Start of post request

--- src/App/Form.js
+++ src/App/Form.js
@@ -16,7 +16,19 @@ export default function Form({addReservation}){
             time, 
             number
         }
+        postReservation(newReservation)
+        console.log(newReservation)
         addReservation(newReservation)
+    }
+
+    function postReservation(newReservation){
+        fetch('http://localhost:3001/api/v1/reservations', {
+            method: 'POST', 
+            headers: {
+                "Content-type": "application/json"
+            }, 
+            body: JSON.stringify(newReservation)
+        })
     }
 
     return (
@@ -24,7 +36,7 @@ export default function Form({addReservation}){
             <input type='text' name='name' value={name} placeholder='Name' onChange={event => setName(event.target.value)}/>
             <input type='text' name='date' value={date} placeholder='Date (mm/dd)' onChange={event => setDate(event.target.value)}/>
             <input type='text' name='time' value={time} placeholder='Time' onChange={event => setTime(event.target.value)}/>
-            <input type='text' name='number' value={number} placeholder='Number of guests' onChange={event => setNumber(event.target.value)}/>
+            <input type='text' name='number' value={number} placeholder='Number of guests' onChange={event => setNumber(Number(event.target.value))}/>
             <button onClick={handleSubmit}>Make Reservation</button>
         </form>
     )
