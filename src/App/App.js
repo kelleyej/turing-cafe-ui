8084e417f8a36fb4d80ac2eec78d48989763fea9
Erin Kelley <erin.kelley01@gmail.com>
2024-04-09 09:21:27 -0600

    Add fetch request function

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,20 @@
 import './App.css';
 import React from 'react';
+import Reservations from './Reservations';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    getReservations()
+  }, [])
+
+  function getReservations(){
+    fetch('http://localhost:3001/api/v1/reservations')
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
+
   return (
     <div className="App">
       <Reservations />
