d052ef8e38db886af66dd179adc1787d3bf82bcc
Erin Kelley <erin.kelley01@gmail.com>
2024-04-09 09:45:02 -0600

    Set state for form component

--- src/App/Form.js
+++ src/App/Form.js
@@ -1,10 +1,17 @@
+import { useState } from 'react';
+
 export default function Form(){
+    const [name, setName] = useState('')
+    const [date, setDate] = useState('')
+    const [time, setTime] = useState('')
+    const [number, setNumber] = useState('')
+
     return (
         <form>
-            <input />
-            <input />
-            <input />
-            <input />
+            <input type='text' name='name' value={name} onChange={event => setName(event.target.value)}/>
+            <input type='text' name='date' value={date} onChange={event => setDate(event.target.value)}/>
+            <input type='text' name='time' value={time} onChange={event => setTime(event.target.value)}/>
+            <input type='text' name='number' value={number} onChange={event => setNumber(event.target.value)}/>
             <button>Make Reservation</button>
         </form>
     )
