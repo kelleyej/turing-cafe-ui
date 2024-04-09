078511a946406c1efc4224ca603dab93f83d3791
Erin Kelley <erin.kelley01@gmail.com>
2024-04-09 09:18:22 -0600

    Add reservations component

--- src/App/App.js
+++ src/App/App.js
@@ -4,11 +4,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1 className='app-title'>Turing Cafe Reservations</h1>
-      <div className='resy-form'>
-      </div>
-      <div className='resy-container'>
-      </div>
+      <Reservations />
     </div>
   );
 }
---
+++ src/App/Reservations.js
@@ -0,0 +1,7 @@
+export default function Reservations(){
+    return (
+        <main>
+            <h1>Test</h1>
+        </main>
+    )
+}
